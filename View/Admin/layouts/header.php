22344f5c0de4253c05b44ae3d13ba56cb9955f15
Barbara Takacs <takacs.barbara97@gmail.com>
2021-10-15 10:27:23 +0200

    session_start() törölve, mert átkerült a page.php fájlba.

--- View/Admin/layouts/header.php
+++ View/Admin/layouts/header.php
@@ -1,4 +1,3 @@
-<?php //session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="hu">
@@ -8,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="./../../Resources/css/Admin/styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
@@ -15,17 +15,3 @@
 </head>
 
 <body>
-
-    
-    <!-- <ul class="nav nav-pills">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Belépés</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Szolgáltatások</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="mainServicesListed.php">Szolgáltatások listázása</a></li>
-                    <li><a class="dropdown-item" href="#">Új szolgáltatás létrehozása</a></li>
-                </ul>
-            </li>
-        </ul> -->
