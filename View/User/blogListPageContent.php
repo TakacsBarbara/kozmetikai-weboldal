4c70a65f5d18db9adb3e9d44bd018daa6c92f05a
Barbara Takacs <takacs.barbara97@gmail.com>
2022-03-19 14:38:17 +0100

    Blogposzt lista oldal kialakítása, formázása.

--- View/User/blogListPageContent.php
+++ View/User/blogListPageContent.php
@@ -3,11 +3,92 @@
     <div class="col-12">
       <div class="blog-list-page">
         <div class="blog-list-title">
-          <h1>Aktuális cikkek</h1>
-          <p></p>
+          <h1>Blog</h1>
         </div>
         <div class="blog-list-content">
           <section class="blog-list-section">
+            <div class="row">
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="blog-post-card">
+                  <div class="blog-post-picture">
+                    <img src="./../../Resources/img/blog-img/bloglist/pilla-tisztitas-blogpost.png" alt="Szempillák tisztítása">
+                  </div>
+                  <div class="blog-post-content">
+                    <div class="blog-post-title">
+                      <h3>Szempillák tisztítása</h3>
+                    </div>
+                    <div class="blog-post-description">
+                      <span>Miért fontos a pilláid ápolása? Ebből a cikkből kiderül, mik a legjobb praktikák szempilláid tisztítására!</span>
+                    </div>
+                    <div class="blog-post-button">
+                      <button type="button">
+                        <a href="./szempillaTisztitasBlog.php">Tovább olvasom</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="blog-post-card">
+                  <div class="blog-post-picture">
+                    <img src="./../../Resources/img/blog-img/bloglist/fenyvedelem-blogpost.png" alt="Fényvédő krém használata napozáskor">
+                  </div>
+                  <div class="blog-post-content">
+                    <div class="blog-post-title">
+                      <h3>Fényvédelem</h3>
+                    </div>
+                    <div class="blog-post-description">
+                      <span>Tudd meg miért fontos a fényvédelem és hogyan válaszd ki a számodra leginkább megfelelő terméket!</span>
+                    </div>
+                    <div class="blog-post-button">
+                      <button type="button">
+                        <a href="./fenyvedelemBlog.php">Tovább olvasom</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="blog-post-card">
+                  <div class="blog-post-picture">
+                    <img src="./../../Resources/img/blog-img/bloglist/szempillaepites-blogpost.png" alt="Műszempilla építés kozmetikában">
+                  </div>
+                  <div class="blog-post-content">
+                    <div class="blog-post-title">
+                      <h3>Szempillaépítés</h3>
+                    </div>
+                    <div class="blog-post-description">
+                      <span>Összefoglaltam nektek, hogy mit kell tudnotok új vendégként, ha műszempillát szeretnétek.</span>
+                    </div>
+                    <div class="blog-post-button">
+                      <button type="button">
+                        <a href="./szempillaepitesElottiTudnivalokBlog.php">Tovább olvasom</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="blog-post-card">
+                  <div class="blog-post-picture">
+                    <img src="./../../Resources/img/blog-img/bloglist/kezapolas-blogpost.png" alt="Kézfejek ápolása krémmel">
+                  </div>
+                  <div class="blog-post-content">
+                    <div class="blog-post-title">
+                      <h3>Kézápolás</h3>
+                    </div>
+                    <div class="blog-post-description">
+                      <span>Nézd meg a legfontosabb kézápolási trükköket, hogy kezeid mindig puhák és ápoltak maradjanak!</span>
+                    </div>
+                    <div class="blog-post-button">
+                      <button type="button">
+                        <a href="./kezapolasBlog.php">Tovább olvasom</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </div>
