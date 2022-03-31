251b5c7badde19fbd48ff2e8ebdfd3a447a38f3a
Barbara Takacs <takacs.barbara97@gmail.com>
2022-03-31 14:47:37 +0200

    Kezdőoldal létrehozása, szerkesztése.

--- View/User/homePageContent.php
+++ View/User/homePageContent.php
@@ -1,3 +1,65 @@
-<div class="">
-  <h1>Home Page</h1>
+<div class="container page-content-container">
+  <div class="row">
+    <div class="col-12">
+      <div class="home-page">
+        <div class="home-quote">
+          <!-- hogy megtalálhasd / megtaláld önmagad
+          A szalon, ahol megtalálod önmagad -->
+
+        </div>
+        <div class="home-content">
+          <section class="home-beauty-salon col-12">
+            <div class="greeting">
+              <div class="greeting-background">
+                <h1>Kedves Látogató!</h1>
+                <h3>Üdvözöllek a Lashes & More by Hegyi Judit weboldalán!</h3>
+              </div>
+            </div>
+            <div class="about-the-salon">
+              <h2>A kozmetikáról</h2>
+              <img src="./../../Resources/img/szalon-1.png" alt="Lashes & More kozmetikai szalon Mosonmagyaróváron">
+              <div class="about-description">
+                <p>Kellemes környezetben várom a szépülni vágyókat, Mosonmagyaróváron a belváros szívében, gyönyörű környezetben található meg szalonom.
+                  Ha szeretnél pár órára kikapcsolódni és feltöltődni, foglalj hozzám időpontot!
+                </p>
+                <p>Két kolléganőmmel közösen üzemeltetjük szalonunkat, így számos lehetőség közül tudsz választani.</p>
+              </div>
+            </div>
+            <div class="our-services">
+              <h2>Szolgáltatásaink</h2>
+              <ul>
+                -szempillahosszabítás
+                -sminktetoválás
+                -kozmetikai kezelések, bőrápolási tanácsadás
+                -nappali és alkalmi sminkek elkészítése
+                -szemöldök laminálás
+                -szempilla lifting
+                -szemöldökformázás
+                -gyantázás
+                -manikűr, műköröm, gél lakk
+                -lézeres tetoválás eltávolítás
+              </ul>
+            </div>
+            <div class="our-greeting">
+              <p>Szeretettel várunk minden érdeklődőt!</p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
+<!-- Termékek 
+Lash flash 
+Lashbrill
+Barbara 
+Szépség gyár 
+Best Lashes 
+Janssen 
+Mesotica 
+Intensive 
+JimJams
+LashesbyLove
+Juca
+Alveola -->
