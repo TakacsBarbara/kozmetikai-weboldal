ee3f7541f488560a62543f69e1d95bb7e984cbc1
Barbara Takacs <takacs.barbara97@gmail.com>
2022-04-05 18:32:38 +0200

    Főoldal létrehozása, formázása.

--- View/User/homePageContent.php
+++ View/User/homePageContent.php
@@ -15,33 +15,291 @@
                 <h3>Üdvözöllek a Lashes & More by Hegyi Judit weboldalán!</h3>
               </div>
             </div>
+            <hr>
             <div class="about-the-salon">
               <h2>A kozmetikáról</h2>
               <img src="./../../Resources/img/szalon-1.png" alt="Lashes & More kozmetikai szalon Mosonmagyaróváron">
               <div class="about-description">
-                <p>Kellemes környezetben várom a szépülni vágyókat, Mosonmagyaróváron a belváros szívében, gyönyörű környezetben található meg szalonom.
-                  Ha szeretnél pár órára kikapcsolódni és feltöltődni, foglalj hozzám időpontot!
+                <p>Kellemes környezetben várom a szépülni vágyókat, Mosonmagyaróváron a belváros szívében, gyönyörű környezetben található meg kozmetikám.
+                  Ha eleged van a zsúfolt, stresszes hétköznapokból, gyere el hozzám szalonomba, ahol nyugalomban tudsz feltöltődni és
+                  szépülni, mert ez a pár óra csak Rólad szól! Foglalj hozzám időpontot!
                 </p>
                 <p>Két kolléganőmmel közösen üzemeltetjük szalonunkat, így számos lehetőség közül tudsz választani.</p>
               </div>
+              <div class="our-greeting">
+                <p>Szeretettel várok minden kedves érdeklődőt!</p>
+                <!-- <p>Szeretettel várunk minden kedves érdeklődőt!</p> -->
+              </div>
+              <button>
+                <a href="./appointmentBooking.php">Online időpontfoglalás</a>
+              </button>
             </div>
+            <hr>
             <div class="our-services">
-              <h2>Szolgáltatásaink</h2>
-              <ul>
-                -szempillahosszabítás
-                -sminktetoválás
-                -kozmetikai kezelések, bőrápolási tanácsadás
-                -nappali és alkalmi sminkek elkészítése
-                -szemöldök laminálás
-                -szempilla lifting
-                -szemöldökformázás
-                -gyantázás
-                -manikűr, műköröm, gél lakk
-                -lézeres tetoválás eltávolítás
+              <h2>Szolgáltatásaim</h2>
+              <div class="row services-home">
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/szempillahosszabbitas.png" alt="Szempillahosszabbítás">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Szempillahosszabítás</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>1D | 2D | 3D műszempillák</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/arckezelesek.png" alt="Arckezelések">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Arckezelés</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>Kozmetikai kezelések, bőrápolási tanácsadás</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/smink.png" alt="Smink">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Smink</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>Nappali és alkalmi sminkek</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/sminktetoválás.png" alt="Sminktetoválás">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Sminktetoválás</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>Szemöldök tetoválások</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/szemoldokformazas.png" alt="Szemöldökformázás">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Szemöldökformázás</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>Szemöldök formázása és festése</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/szemoldoklaminalas.png" alt="Szemöldöklaminálás">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Szemöldöklaminálás</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>A szemöldökök tartós formázása</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/szempillalifting.png" alt="Szempilla lifting">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Szempilla lifting</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>Hosszan tartó szempillaformázás</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                  <div class="blog-post-card">
+                    <div class="blog-post-picture">
+                      <img src="./../../Resources/img/services/gyanta.png" alt="Gyantázás">
+                    </div>
+                    <div class="blog-post-content">
+                      <div class="blog-post-title">
+                        <h3>Gyantázás</h3>
+                      </div>
+                      <div class="blog-post-description">
+                        <span>Bajusz | Áll | Hónalj | Kar | Láb</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p>Kattints a gombra és olvasd el az egyes szolgáltatások részletes leírását!</p>
+                <button><a href="./aboutServicesPage.php">Tovább olvasok</a></button>
+              </div>
+              <!-- <h2>Szolgáltatásaink</h2> -->
+              <!-- -manikűr, műköröm, gél lakk
+                -lézeres tetoválás eltávolítás -->
               </ul>
             </div>
-            <div class="our-greeting">
-              <p>Szeretettel várunk minden érdeklődőt!</p>
+          </section>
+          <section class="guests-said">
+            <div class="container">
+              <div class="overlay"></div>
+              <div class="title">
+                <h2>Vendégeim véleménye</h2>
+              </div>
+              <div id="carouselExampleDark" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-bs-interval="8000">
+                    <img src="./../../Resources/img/guests-said-about/vivi.png" class="d-block w-70" alt="Vivien véleménye rólam">
+                  </div>
+                  <div class="carousel-item" data-bs-interval="8000">
+                    <img src="./../../Resources/img/guests-said-about/laura.png" class="d-block w-70" alt="Laura véleménye rólam">
+                  </div>
+                  <div class="carousel-item" data-bs-interval="8000">
+                    <img src="./../../Resources/img/guests-said-about/petra.png" class="d-block w-70" alt="Petra véleménye rólam">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </section>
+          <section class="why-choose-me">
+            <div class="title">
+              <h2>Ezért válassz engem!</h2>
+            </div>
+            <div class="content">
+              <div class="row">
+                <div class="col-12 col-md-4 picture-container">
+                  <img src="./../../Resources/img/why-choose-me/termeszetes-hozzavalok.png" alt="Természetes alapanyagokból készült termékek">
+                </div>
+                <div class="col-12 col-md-8 reasons">
+                  <div class="reasons-container">
+                    <div class="reason-title">
+                      <h2>Természetes alapanyagok</h2>
+                    </div>
+                    <div class="reason-description">
+                      <p>Kizárólag bőrbarát, természetes alapanyagokból készült termékekből választom ki a számodra leginkább megfelelő krémet, arctisztítót a kezelések során.</p>
+                      <p>Kérj ingyenes konzultációt, ahol átbeszéljük, milyen termékek illenek a Te bőrtípusodhoz!</p>
+                    </div>
+                    <div class="free-consultation-btn">
+                      <button id="free-consultation-btn"><a href="#free-consultation-form">Ingyenes konzultációt kérek</a></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 col-md-8 reasons">
+                  <div class="reasons-container">
+                    <div class="reason-title">
+                      <h2>Megfizethető árak</h2>
+                    </div>
+                    <div class="reason-description">
+                      <p>Szolgáltatásaim kedvező árakon érhetők el szalonomban minden szépülni vágyó vendégem számára!</p>
+                      <p>Nézd meg az elérhető szolgáltatásokat és áraikat!</p>
+                    </div>
+                    <div class="check-prices-btn">
+                      <button id="check-prices-btn"><a href="./priceListPage.php">Tovább az árakhoz</a></button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-md-4 picture-container">
+                  <img src="./../../Resources/img/why-choose-me/kedvezo-arak.png" alt="Megfizethető árak">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 col-md-4 picture-container">
+                  <img src="./../../Resources/img/why-choose-me/professzionalis-termekek.png" alt="Professzionális termékek">
+                </div>
+                <div class="col-12 col-md-8 reasons">
+                  <div class="reasons-container">
+                    <div class="reason-title">
+                      <h2>Professzionális termékek</h2>
+                    </div>
+                    <div class="reason-description">
+                      <p>A természetes és kiváló minőségű kozmetikai termékek, melyekkel dolgozom nem csak táplálóak, de igazi luxus élményt nyújtanak számodra.</p>
+                      <p>Gyere el hozzám és kapcsolódj ki pár órára! Foglalj online időpontot!</p>
+                    </div>
+                    <div class="professional-products-btn">
+                      <button id="professional-products-btn"><a href="./appointmentBooking.php">Időpontot foglalok</a></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="brands">
+
+            </div>
+          </section>
+          <section id="free-consultation-form" class="send-message-form">
+            <div class="container">
+              <div class="send-message-overlay"></div>
+              <div class="container user-login-form-style">
+                <div class="row">
+                  <div class="col-12 col-md-9 col-lg-6 user-msg-form">
+                    <div class="user-msg-title">
+                      <h2>Kérj ingyenes konzultációt!</h2>
+                    </div>
+                    <form method="POST" action="">
+                      <div class="mb-3">
+                        <input type="text" class="form-control" id="user-msg-name" placeholder="Név*">
+                      </div>
+                      <div class="mb-3">
+                        <input type="text" class="form-control" id="user-email-address" placeholder="Email cím*">
+                      </div>
+                      <div class="mb-3">
+                        <input type="phone" class="form-control" id="user-phone" placeholder="Telefonszám*">
+                      </div>
+                      <div class="mb-3">
+                        <textarea id="user-msg" class="form-control" placeholder="Üzenet szövege*" cols="30" rows="5" maxlength="750" style="resize: none;"></textarea>
+                      </div>
+                      <div id="resultMessage"></div>
+                      <div class="mb-3" id="user-send-msg-btn-container">
+                        <button type="button" class="user-login-btn" id="user-send-msg-btn">Küldés</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
           </section>
         </div>
