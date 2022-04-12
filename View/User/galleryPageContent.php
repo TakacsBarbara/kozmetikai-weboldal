655f34fcbdbcdaae8daae65e9923560beb71524c
Barbara Takacs <takacs.barbara97@gmail.com>
2022-04-12 22:31:06 +0200

    Galéria létrehozása.

--- View/User/galleryPageContent.php
+++ View/User/galleryPageContent.php
@@ -1,3 +1,74 @@
-<div class="">
-  <h1>Galéria</h1>
+<div class="container page-content-container">
+  <div class="row">
+    <div class="col-12">
+      <div class="gallery-page">
+        <div class="gallery-title">
+          <h1>Galéria</h1>
+        </div>
+        <div class="gallery-content">
+          <section class="gallery-section">
+            <div class="row">
+              <div class="col-12">
+                <h2>Tekintsd meg eddigi munkáimat!</h2>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div id="carouselExampleDark" class="carousel carousel-dark slide gallery-carousel" data-bs-ride="carousel">
+                  <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                  </div>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/smink1.png" class="d-block w-70" alt="Lila smink">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/pilla1.png" class="d-block w-70" alt="Szempillaépítés">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/laminalas1.png" class="d-block w-70" alt="Szemöldöklaminálás">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/tetko1.png" class="d-block w-70" alt="Sminktetoválás">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/pilla5.png" class="d-block w-70" alt="Szempillaépítés">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/laminalas2.png" class="d-block w-70" alt="Szemöldöklaminálás">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                      <img src="./../../Resources/img/gallery/pilla4.png" class="d-block w-70" alt="Szempillaépítés">
+                    </div>
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <p class="book-appointment-lashes">Foglalj hozzám egy időpontot, ha felkeltette az érdeklődésed!</p>
+                    <div class="szempillahosszabitas-btn">
+                      <button id="szempillahosszabitas-btn"><a href="./appointmentBooking.php">Időpontot foglalok</a></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
